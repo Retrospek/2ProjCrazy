c482d6df353b184d8d9bb027fef9a1da0cceb9f2
Srihari Yechangunja <69185362+fgmx1653@users.noreply.github.com>
2024-11-10 11:10:45 -0600

    Frontend

---
+++ ronis/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/_fgmx/Downloads/datathon/2ProjCrazy/ronis/app/layout\":[\"static/media/463dafcda517f24f-s.p.woff\",\"static/media/4473ecc91f70f139-s.p.woff\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
